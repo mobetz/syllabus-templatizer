82366742da61fb905584a0cfee0e51dbb2411223
Matthew Obetz <mobetz@gmail.com>
2023-09-25 13:24:46 -0400

    new booleans lecture

--- code-lectures/CS120/week4/booleans/Booleans.java
+++ code-lectures/CS120/week4/booleans/Booleans.java
@@ -16,7 +16,6 @@ Vocabulary for the Day
   Boolean - A boolean is a type of data that specializes in storing the 'truthiness' of certain conditions. Boolean data
       can only have one of two possible values: true or false.
 
-
   Boolean operator - A boolean operator is a special symbol that lets us combine two booleans together. The three main
       boolean operators are NOT, AND, and OR.
 
@@ -25,17 +24,19 @@ Vocabulary for the Day
       a certain 'condition' is met.) Branches (if-statements) and Loops (for-loops and while-loops) are the most common
       types of conditional logic.
 
+
   Control flow - "Control flow" is a term we use to talk about any instructions we can write that change the order
       other instructions run. Conditional logic, functions, and exception handling are all control flow!
  
  */
 
-
 import java.io.File;
+import java.lang.Thread;
 
-public class BooleanLogic { 
+public class Booleans {
+	
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         /* So far, we've learned about a few important types of data:
             - numeric types, that let us perform calculations and store numbers in a space efficient way.
@@ -46,8 +47,8 @@ public class BooleanLogic {
          similarly to all the other base types we've seen this semester:
          */
 
-         boolean is_today_sunny;
-         is_today_sunny = true;
+        int x = 4;
+        String name = "Jim";
 
 
 
@@ -56,15 +57,21 @@ public class BooleanLogic {
            - If the answer is yes, we save the value true.
            - If the answer is no, then we save the value false.
 
+           */
 
+
+        boolean is_today_sunny;
+        is_today_sunny = false; //<- no, it is not sunny
+
+        /*
         Some of the objects we've already worked with this semester have useful boolean methods we can access to
         ask some common questions!
          */
 
-
-         String a_word = "apple";
-         boolean ends_with_ple = a_word.endsWith("ple");
+        String a_word = "apple";
+        boolean ends_with_ple = a_word.endsWith("ple");
         System.out.println("Does " + a_word + " end with 'ple'?: " + ends_with_ple);
+
 
 
         File a_file = new File("some_file_that_doesnt_exist.txt");
@@ -72,17 +79,20 @@ public class BooleanLogic {
         System.out.println("Does " + a_file.getPath() + " exist?: " + does_file_exist);
 
 
+
+
         /* Sometimes, we want to ask yes/no questions that have more than one part.
+
 
         For a rainbow to appear, we need weather that is both sunny AND ALSO rainy. We can ask these questions using
         special symbols called "boolean operators", which allow us to chain together simpler questions:
          */
 
-        boolean is_today_rainy = false;
+        boolean is_today_rainy = true;
 
-
-        boolean will_rainbow_appear =  is_today_sunny && is_today_rainy;
+        boolean will_rainbow_appear = is_today_sunny && is_today_rainy;
         //will_rainbow_appear will only be true if is_today_sunny is true AND is_today_rainy is also true.
+
 
         System.out.println("Sunny: " + is_today_sunny + " Rainy: "  + is_today_rainy + 
          ", Will rainbow appear? " + will_rainbow_appear);
@@ -92,38 +102,41 @@ public class BooleanLogic {
 
         /* There are three boolean operators we will talk about during this class: AND, OR, and NOT.
 
-
            For each boolean operator, we can create a "truth table". A truth table shows all possible combinations of
            inputs for a given boolean operator, and describes what the output will be.
-
 
 
            AND is the operator we use when we want to test if multiple conditions are ALL met. We express AND in Java
            using a double ampersand (&&).
 
+
                                                   Y
                              AND (&&) |   True    |   False   |
                                  -----+-----------+-----------+
                                  True |   True    |   False   |
-                           X     -----+-----------+-----------+
-                                False |  False    |   False   |
-                                      +-----------+-----------+
+                            X   -------------------------------
+                                False |   False   |   False   |
+                               --------------------------------
+
 
 
            OR is the operator we use when we want to test if ANY condition is met. We express OR in Java
            using a double pipe (||).
 
+
+
                                                   Y
                               OR (||) |   True    |   False   |
                                  -----+-----------+-----------+
                                  True |   True    |    True   |
-                           X     -----+-----------+-----------+
+                            X   -------------------------------
                                 False |   True    |   False   |
-                                      +-----------+-----------+
-
+                             ----------------------------------
+                                   
+                                   
 
            NOT is the operator we use when we want to test if a single condition is UNMET. We express NOT in Java
-           using an exclamation point (!).
+           using an exclamation point (!).   
 
 
                                     NOT (!)  |  Result   |
@@ -136,13 +149,17 @@ public class BooleanLogic {
 
 
              In addition to our three boolean operators, we also have a few operators that help us compare things and
-             get a boolean answer. We call these the 'relational operators'. The relational operators we'll use in this
-             class are:
+             get a boolean answer. We call these the 'relational operators'.
+
+              The relational operators we'll use in this class are:
+
 
                      ==                  (equality) - the equality operator tests if the things on both sides are the
                                                       same. For example: (2 + 2) == 4 will evaluate to true.
                                                       NOTE: for objects, we need to use the .equals() function instead
                      !=                (inequality) - the inequality operator tests if both sides are NOT the same.
+
+
 
 
                       >              (greater than)
@@ -153,21 +170,22 @@ public class BooleanLogic {
                                                         4 < 5 will be true, but 4 >= 5 will be false.
 
 
+
              One important feature of boolean operators is that, just like with arithmetic operators like + and *,
              we can string multiple pairs together, and they even have their own order of operations!
 
 
-
-             In Java, the order of operations for booleans is:
                 1) Parentheses ( ).
                 2) NOT.
                 3) relational operators like >, <, ==, and !=; left to right.
                 4) AND, left to right.
                 5) OR, left to right.
 
+
              Since parentheses are the highest precedence, we can use them the same way that we use them in math, to
              group things together to be done first.
          */
+
 
 
         /* Let's test out how well we understand our boolean operators and order of operations with a poll question:
@@ -175,15 +193,18 @@ public class BooleanLogic {
          */
         boolean is_this_true = true && !true || !false == !(false || !false);
         /*
-                is_this_true = true && !true || !false == !(false || true);
-                is_this_true = true && !true || !false == !true;
-                is_this_true = true && false || true == false;
-                is_this_true = true && false || false;
-                is_this_true = false || false;
-                is_this_true = false;
-                */
+                is_this_true =  true && !true || !false == !(false || true)
+                is_this_true =  true && !true || !false == !true
+                is_this_true =  true && false || true == false
+                is_this_true =  true && false || false
+                is_this_true =  false || false
+                is_this_true =  false
+
+
+        */
 
         System.out.println("The answer is: " + is_this_true);
+
 
 
         /*
@@ -196,46 +217,46 @@ public class BooleanLogic {
         written similarly:
 
 
+
         KEYWORD ( boolean_expression ) {
               //stuff that only happens conditionally
         }
 
-
-
         The difference between our different types of conditional statements will be in what happens before/after we
         test our condition.
+
 
         The first type of control flow we will look at are branches.
         To make a branch, we use the keyword "if" to say "Only do these instructions IF a boolean condition is met":
          */
 
-
         if ( is_today_rainy ) {
-            System.out.println("Don't forget your umbrella!");
+        	System.out.println("Don't forget your umbrella!");
         }
+
+
+		if ( is_today_sunny ) {
+			System.out.println("Don't forget your sunscreen!");
+		}
+
+
 
         /* In this example, we will only print a reminder about an umbrella when today is rainy. Otherwise, we will skip
-        everything inside our curly braces entirely.
-        */
-
-        if ( is_today_sunny ) {
-         System.out.println("Don't forget sunscreen!");
-        }
-
-        /*
+        everything inside those curly braces entirely.
 
         We can even use branches with our compound boolean expressions:
          */
 
+		if ( is_today_sunny && is_today_rainy ) {
+			System.out.println("Look for a rainbow!");
+		}
 
-        if ( is_today_sunny && is_today_rainy ) {
-            System.out.println("Look for a rainbow!");
-        }
 
 
         /* On Wednesday we'll look at some of the fancy things we can add to our if statements to make them more
          expressive, but for today, we want to compare these if statements to one other type of conditional statement:
            a loop.
+
 
          A loop works just like an if statement, with one important difference: at the end of a loop, we'll go back to
          the top of our loop and ask the same question again!
@@ -244,28 +265,24 @@ public class BooleanLogic {
          will use the word "while" instead of if:
          */
 
+		int some_counter = 0;
+		while ( some_counter < 10 ) {
+             System.out.println("Some counter is now: " + some_counter);
+             some_counter = some_counter + 1;
+             // Thread.sleep(1000);
 
-
-        int some_counter = 0;
-        while ( some_counter < 10 ) {
-            System.out.println("Some counter is now: " + some_counter);
-        }
-
+		}
 
         /*
          Unlike our if statement, which just checks if something happened once, our while loop is going to check over
          and over and over again, until the condition isn't true anymore. Because of this, it's going to repeat the body
          of our loop and print out the value of "some_counter" several times!
 
+
          Because we will keep checking our condition until that condition is false, it is VERY IMPORTANT that something
          somewhere in our loop has the potential to change the value of our expression.
 
          */
-
-   }
+    }
 
 }
-
-
-
-
