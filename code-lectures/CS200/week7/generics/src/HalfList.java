b783f99a5de8525d09545138d0ac9229580647c4
Matthew Obetz <mobetz@gmail.com>
2023-10-19 13:23:24 -0400

    Add this week's lectures

--- code-lectures/CS200/week7/generics/src/HalfList.java
+++ code-lectures/CS200/week7/generics/src/HalfList.java
@@ -1,19 +1,23 @@
-
-
 import java.util.Iterator;
 import java.util.List;
 
 public class HalfList implements Iterator {
 
-    private List<Integer> inner_list;
+
+    //ATTRIBUTES
+    List<Integer> inner_list;
     private boolean evens_only;
     private int current_position;
 
+
+    //METHODS
     public HalfList(List<Integer> original_elements) {
         this.inner_list = original_elements;
         this.evens_only = true;
         this.current_position = 0;
     }
+
+
     public HalfList(List<Integer> original_elements, boolean prioritize_evens) {
         this.inner_list = original_elements;
         this.evens_only = prioritize_evens;
@@ -25,20 +29,25 @@ public class HalfList implements Iterator {
 
     It turns out, there's an interface for exactly this: Iterator!
      */
+
+
+    @Override
     public Integer next() {
-        int returned_item =  this.inner_list.get(this.current_position);
+        int returned_item = this.inner_list.get(this.current_position);
         this.current_position += 2;
 
         return returned_item;
-
     }
 
+    @Override
     public boolean hasNext() {
         //Determine whether there is one "extra element" (e.g a list of 9 things when we're only returning evens):
-            int index_offset = this.inner_list.size() % 2;
-            index_offset = index_offset + (( this.evens_only ) ? 0 : 1);
-            int last_index = this.current_position + index_offset;
+              int is_odd_offset = this.inner_list.size() % 2;
+              is_odd_offset = is_odd_offset + (( this.evens_only ) ? 0 : 1);
+              int last_index = this.current_position + is_odd_offset;
 
-        return this.inner_list.size() >= last_index;
+        boolean past_the_end_of_the_list = this.inner_list.size() >= last_index;
+        return past_the_end_of_the_list;
     }
+
 }
