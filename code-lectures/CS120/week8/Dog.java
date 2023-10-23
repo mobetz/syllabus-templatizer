e8374fd4679c0f237a083424a63d9706ec0b577c
Matthew Obetz <mobetz@gmail.com>
2023-10-23 15:37:16 -0400

    new version of review lecture

--- code-lectures/CS120/week8/Dog.java
+++ code-lectures/CS120/week8/Dog.java
@@ -1,6 +1,8 @@
 
+
 /*
 A class is a blueprint or "template" that describes a group of data and actions that belong bundled together.
+
 
 Classes have two main parts:
         - The "attributes" describe what data a class needs to save.
@@ -12,13 +14,12 @@ of the attributes and methods.
 
 Classes are defined using the 'class' keyword. All the code related to the class must go inside the curly braces. */
 
-public class Dog {
-	
-	
-    /* ATTRIBUTES
-    Each attribute is a variable.
 
-    Just like any other variable, attributes must be declared by giving Java a type and a name.
+public class Dog {
+
+	//ATTRIBUTES
+	/*
+    Each attribute is a variable.
 
     However, attributes have one more keyword: public/private.
        - Private attributes can only be seen inside the methods of the class.
@@ -26,6 +27,8 @@ public class Dog {
 
     We usually prefer private attributes, because they let us keep full control over our objects and ensure that the
     methods of the class are the only place those attributes can change.
+
+    All of the attributes of our class each represent one "part" of the larger concept we're writing a class for:
     */
 
     private double age;
@@ -33,50 +36,52 @@ public class Dog {
     private String breed;
 
 
+	//METHODS
+	/*
 
-    /* METHODS
-    Each method describes a thing our class is able to do.
+    Each method describes a thing our class is able to do, written as a function.
+
 
     As we described before, methods need to communicate to Java:
         - the things they need given to them by a programmer in order to run (the inputs/parameters/arguments)
         - the thing they will give back after they are finished (the return value)
         - the name programmers can use to call them
 
+
     We put all of this information in a single line at the top of the method, called the "method signature".
     Like attributes, methods can also be public or private:
      */
-     public boolean changeName(String newName) {
+    public boolean changeName(  String newName  ) {
         /*this method signature says:
            - We need one thing to change a dog's name: the new name of the Dog (as text)
            - We give back one thing when we're done: a true or a false (true if the name change succeeded)
+
 
            Each of our input parameters is a declared variable, just like any other declared variable. This means we need
            a type and a name. The value in the parameters can change every time the function is called, and they
            get thrown away at the end of the method.
          */
 
-     	boolean name_change_succeeded = false;
+    	boolean name_change_succeeded = false;
 
-     	String first_letter = newName.substring(0,1);
-     	if ( first_letter.equals(first_letter.toUpperCase())) {//Only allow dogs with capitalized names
-     		
-     		this.name = newName;
-        // Remember: the "this" keyword refers to the object that we're currently calling this method on.
+    	String first_letter = newName.substring(0,1);
+    	if ( first_letter.equals(first_letter.toUpperCase())) { //<- check that the new name has a capitalized first letter
+     		this.name = newName; //<- only dogs with capitalized names will be supported here
+     		 // Remember: the "this" keyword refers to the object that we're currently calling this method on.
 
 
-        // Using it ensures we're ALWAYS talking about an attribute on the object we're calling this method on.
-        // if we don't put it, Java will first look for a local variable with that name, then it will check for
-        // an attribute afterward.
+        	// Using it ensures we're ALWAYS talking about an attribute on the object we're calling this method on.
+        	// if we don't put it, Java will first look for a local variable with that name, then it will check for
+        	// an attribute afterward.
 
      		name_change_succeeded = true;
-     	}
+    	}
+
 
         //The last statement in a method that gives something back to the programmer who called it is the "return" statement.
         //this statement describes which variable the method is giving back to fulfill its promise.
-     	return name_change_succeeded;
-
-
-     }
+        return name_change_succeeded;
+    }
 
 
 
@@ -89,7 +94,8 @@ public class Dog {
 
      The name of the constructor method is always exactly the same as the name of the class:
      */
-     public Dog(String given_name, String given_breed) {
+
+    public Dog(String given_name, String given_breed) {
 
         //The parameters of the constructor are all the things a programmer must give us when they want to make a new
         // object of this class.
@@ -102,8 +108,9 @@ public class Dog {
         // Sometimes, we also have default values that we can calculate in the constructor without input from the programmer.
         this.age = 0;
 
-     }
+        //Normally, by the end of a constructor, every single attribute should have a value (nothing should be left as "null" or 
+        // as a dummy placeholder).
 
-
-	
+        //Just as with our method signature, constructors don't need to explicitly reutrn anything.
+    }
 }
--- code-lectures/CS120/week8/Review.java
+++ code-lectures/CS120/week8/Review.java
@@ -12,9 +12,11 @@ Today, we will:
  */
 
 
+
 public class Review {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //<- remember, the "args" here are command line arguments -- things that come in from the terminal when you type "java <program_name> <arg1> <arg2> <arg3>"
+
 
         /* Declaration is the process of creating a new variable. A variable is a named slot that lets us store
         data so that we can use it more than once.
@@ -22,8 +24,8 @@ public class Review {
         When we declare a variable, we need to tell Java two things:
          */
 
-        int some_number;
 
+        int some_number;
 
         /* We need to tell Java what kind of value we want to save, and then we need to give that value a name.
 
@@ -33,6 +35,7 @@ public class Review {
 
         The simplest types are base types, also called "primitives". These are types that are included in Java by default:
         */
+
 
         boolean a_bit;       //stores one bit. Typically used to represent "True" or "False"
         byte a_byte;      //stores 8 bits numerically. Can represent values from -128 to 127.
@@ -49,23 +52,28 @@ public class Review {
         /* However, variables can hold more kinds of values than just our primitives. We can also make a variable to
         hold a whole bundle of related data in a single place.
 
+
         We call these bundles of data "objects".
+
 
         The type for these variables is the name of the object's "class", but otherwise object
          variables are declared just like any other variable: */
 
-         File a_file_handle;
-         String some_text;
-         Scanner input_reader;
-         Assignment some_graded_work;
+        File a_file_handle;
+        String some_text;
+        Scanner input_reader;
+        Assignment some_graded_work;
+
 
 
            /* Assignment is the process of saving a value into a variable. We perform assignment with the 
            equals operator (=).
 
+
         Assignments have two parts:
          - To the left of the equals sign, we put the place we want to save something.
          - To the right of the equals sign, we put the thing we want to save.
+
 
         The simplest assignments are those where we assign a "literal value" into a variable:
          */
@@ -74,15 +82,18 @@ public class Review {
          a_letter = 'x';
          some_text = "Hello world!";
 
+
         /*
         Anywhere that we can use a literal value in a program, we can also use any "expression" that will
         turn into a value when it's finished:
          */
 
+
         some_number = 4 + 6;
         a_bit = (true || !(false && true));
         some_text = "Hello, " + "World!";
         medium_num = some_number + 1;
+
 
         /* When we have one of these value expressions, Java starts on the right hand side, does all that work, then
          * saves the final answer on the left. */
@@ -99,21 +110,22 @@ public class Review {
         called with ----------> |  some_function  | --------->   assigned to a variable
          arguments              +-----------------+    return
 
-
             To call a method, we put the name of that method, then all the arguments that method needs inside parentheses:
          */
 
-        System.out.println("This text is the single argument to the println() method.");
+         System.out.println("This text is the single argument to the println() function.");
 
-        /* When a method gives back a return value after it finishes, we can save that return value in a variable. */
-         big_dec = Math.pow(2, 4);
+
+        /* When a method gives back a return value after it finishes, it is an expression just like any other work.
+        This means we can save that return value in a variable. */
+
+        big_dec = Math.power(2, 4);
 
 
         /* Just like with our assignment above, any argument to a method that can be a literal value can also be a
          value expression: */
 
-         some_text = some_text.repeat( some_number + 1 );
-
+         some_text = some_text.repeat(some_number + 1);
 
         /* Even method calls themselves count as value expressions: */
          System.out.println( some_text.repeat(Math.abs(-4)) );
@@ -123,19 +135,26 @@ public class Review {
           This works like any other method call, but it has one extra keyword, new:
          */
 
-          a_file_handle = new File("some/path/to/the/file.txt"); 
+         a_file_handle = new File("some/path/to/the/file.txt");
           some_graded_work = new Assignment(50, 50, "lab"); 
           input_reader = new Scanner( System.in );
           ///the "new" keyword tells Java to save room for all the attributes!
 
+
         /*
         OBJECTS AND CLASSES
 
-        Once we have an object saved in a variable, we can tell that object to do something with the dot operator.
+
+        An object is a bundle of variables with functions created from a class "blueprint". 
+        Once we have an object saved in a variable, the main way we interact with those variables is by calling
+        its methods/functions.
+
+        We can tell that object to do something with the dot operator.
 
         On the left of the dot, we put the object we are talking to.
         On the right of the dot, we put the thing we want to use from an object (usually a method.)
          */
+
 
           some_text = a_file_handle.nextLine();
           medium_num = some_graded_work.getPointsPossible();
@@ -149,11 +168,12 @@ public class Review {
 
           String shortened_replaced_line_from_user = input_reader.nextLine().replace(" ", "_").substring(0, 20);
 
+          String answer = "This is a very long line of text".substring(0,5); //<- this will save just the first five letters
+          System.out.println(answer); //<- this will print out "This ";
           // This is the same as writing:
           String text_from_user = input_reader.nextLine();
           String replaced_text = text_from_user.replace(" ", "_");
           String shortened_text = replaced_text.substring(0, 20);
-
 
 
         /* Java comes with many different classes of objects, but often we'll have to make our own to solve the specific
@@ -165,41 +185,48 @@ public class Review {
          */
 
           Dog a_dog = new Dog("Spot", "Beagle");
+          a_dog = new Dog("SecondDog", "Husky"); //<- I can still reassign into a object variable like any other
+                                                 // just like with base types, this "deletes" everything previously there
           a_dog.changeName("Tricksy");
 
 
         /*
         CONDITIONALS
 
+
         Sometimes, we want to only execute instructions when a particular condition is met.
 
         When this is the case, we can use an if statement.
          */
 
-
         boolean some_condition_is_met = true; //this could also be from a function or one of our boolean operators...
 
-        if (some_condition_is_met) {
+        if ( some_condition_is_met ) {
             //The code inside these curly braces will only run when the value expression inside the parentheses is true.
 
             //Just like with our assignments and method calls, the value expression can be anything that will eventually
             // become either "true" or "false", whether it's a variable, or a comparison with < or ==, or a method call.
+
+            //REMINDER: you can only use the == with base types. For objects (including "Strings",) you need to use .equals()
         } else {
+
             //things inside an "else" statement will only happen when the things in their preceding if statement DON'T run
             // (when the conditional expression is "false".)
         }
 
-
         /*
         LOOPS
+
         Sometimes, we want to keep repeating some instructions AS LONG AS a condition is met, over and over.
 
         When this happens, we can use a loop. We have two different keywords for loops:
 
+
         A WHILE loop lets us repeat instructions until a particular condition is no longer true:
          */
 
-        while (some_condition_is_met) {
+        while ( some_condition_is_met ) {
+
             /*
             The first time a program reaches a while loop, it behaves just like an if statement --
             if the condition is met, the code inside the curly braces executes.
@@ -220,9 +247,11 @@ public class Review {
               - declare a variable that will keep count of which numbered time we are executing the loop.
               - write a condition (just like our while loop)
               - write a statement that updates our variable keeping count (usually using ++ to add 1 to the variable we declared.)
-         */
 
-        for (int number_of_runs=0; number_of_runs < 5; number_of_runs = number_of_runs + 1) {
+        */
+
+        for ( int number_of_runs=0;    number_of_runs < 5 ;  number_of_runs = number_of_runs + 1 ) {
+
             //Our variable keeping count of the loop is a variable, just like any other.
             // We need to declare the loop variable, and assign it an initial value.
 
@@ -230,20 +259,24 @@ public class Review {
             // At the very end of the loop, the update statement runs, then the condition is checked again.
         }
 
-
         /*
         It's possible to rewrite any for loop as a while loop by declaring the variable before the loop ourselves, and
         update the variable at the end of the loop:
          */
 
         int number_of_runs=0;
-        while ( number_of_runs < 5 ){
-            number_of_runs = number_of_runs + 1;
+        while ( number_of_runs < 5 ) {
+          //Whatever happens in the body goes here...
+
+          number_of_runs = number_of_runs + 1;
         }
 
 
         /* While loops written using this pattern can also be converted back into for loops (but not all while loops
-             follow this pattern)! */
+             follow this pattern)! Some might execute an unknown number of times (like repeatedly asking for input
+             until it's correct.) */
+
+
 
 
         //ARRAYS
@@ -253,7 +286,8 @@ public class Review {
         Arrays let us store groups of things that are all the same type, such as multiple datapoints:
         */
 
-        Dog[] a_whole_kennel_of_dogs;
+        Dog[] a_whole_kennel_of_dogs; //<- the type of an array is the same as one thing in the array, with []
+
 
 
         /*
@@ -270,26 +304,55 @@ public class Review {
         a_whole_kennel_of_dogs[1] = new Dog("Roody", "Lab");
 
 
+
         /*
         We use the same notation to get things back from the array:
          */
-        Dog the_second_dog = a_whole_kennel_of_dogs[1]; 
+
+        Dog the_second_dog = a_whole_kennel_of_dogs[1];
+
 
 
         /*
         We even have a special type of loop to traverse each item in this array:
         */
-
         for ( Dog one_dog : a_whole_kennel_of_dogs ) {
             /*
             A For-each loop has two parts, separated by a colon:
             - On the left of the colon, we put a new variable that can hold one item from the group
             - On the right of the colon, we put the group that we want to pull things from
-            
+
             The loop repeats once for each thing in the array!
             */
             System.out.println(one_dog);
+            one_dog = new Dog("Spot", "Dalmation"); //<- THIS WILL NOT ACTUALLY GET SAVED INTO A_WHOLE_KENNEL_OF_DOGS.
+                                                    // one_dog is a Dog variable, completely separate from the kennel array.
+                                                    // when we reassign it, we're "deleting" the value that was pulled from the array, 
+                                                    // ...but this still isn't assigning it to the array.
+            /*
+
+              whole_kennel:
+             +----+----+----+----+----+----+----+----+----+----+
+             |    |    |    |    |    |    |    |    |    |    | 
+             +----+----+----+----+----+----+----+----+----+----+
+                    ^
+                    The dog in slot two
+                     \
+                     \
+                     \
+                    +---+
+                    |   | <- if I reassign what's in this box, I'm "scratching out" the arrow above and overwriting the one_dog variable,
+                    +---+       losing any association with the array at all.
+                   one_dog
+
+          If you actually want to permanently change the array, you need to use a for loop like the one below
+          and assign to the kennel[slot].
+            */
+            /*
+            Remember: we will still loop through "empty" array slots. The value in one_dog will be null for each empty slot.
+            */
         }
+
 
 
         /*
@@ -301,7 +364,33 @@ public class Review {
 
             System.out.println(one_dog); //<- this does the same thing as the previous example!!!
         }
-        
+
+
+
+        /*
+        Scanners
+
+
+        A scanner is an object that knows how to read from an input source.
+
+        Scanners are typically used to read from one of two things: either a File object, or the command line.
+
+        In both cases, they have exactly the same methods:
+        */
+
+        Scanner in = new Scanner(System.in); //<- the constructor is told what it is going to read from. System.in is the terminal input.
+
+        String next_line = in.nextLine(); //<- this will pause and wait to read one line up to a carriage return (the enter key), or one line of a file.
+
+        /*
+        One common way that these objects are used is reading "as long as" there is something left to be read:
+        */
+
+        while ( in.hasNext() ) { //<- keep reading as long as there are lines remaining. Mostly used with Files, not input.
+          String next_line = in.next_line(); 
+        }
+
     }
 
 }
+
