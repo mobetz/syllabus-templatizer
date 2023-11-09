c857a8d025990ef24ff8c5afdaf25ede047a55ce
mobetz <mobetz@gmail.com>
2023-11-08 22:01:23 -0500

    rest of files draft

--- code-lectures/CS200/week10/watching-files/FileListener.java
+++ code-lectures/CS200/week10/watching-files/FileListener.java
@@ -1,4 +1,5 @@
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.io.*;
 import java.nio.*;
@@ -9,10 +10,9 @@ public class FileListener implements Runnable {
 	
 	private WatchService watcher;
 	private File watched_file;
-	private List<String> file_updates;
 
 
-	private int last_position;
+	private long last_position;
 	private RandomAccessFile open_file;
 
 
@@ -23,31 +23,54 @@ public class FileListener implements Runnable {
 	public void run() {
 		System.out.println("Starting the file watcher:");
 
-		try { 
+		try {
+			this.open_file = new RandomAccessFile(this.watched_file, "rw");
+			this.last_position = this.open_file.getFilePointer();
 
-		this.watcher = FileSystems.getDefault().newWatchService();
-		this.watched_file.toPath().register(this.watcher, StandardWatchEventKinds.ENTRY_MODIFY);
-		boolean watching_file = true;
-		while (watching_file) {
-			WatchKey key = this.watcher.take();
-			List<WatchEvent<?>> things_that_happened = key.pollEvents();
-			for ( WatchEvent<?> event : things_that_happened) {
+			this.watcher = FileSystems.getDefault().newWatchService();
+			Path folder = this.watched_file.toPath().toAbsolutePath().getParent();
 
-				List<String> lines = Files.readAllLines(this.watched_file.toPath());
-				System.out.println(lines);
+			folder.register(this.watcher, StandardWatchEventKinds.ENTRY_MODIFY);
+			boolean watching_file = true;
+			while (watching_file) {
+				WatchKey key = this.watcher.take();
+				List<WatchEvent<?>> things_that_happened = key.pollEvents();
+				for (WatchEvent<?> event : things_that_happened) {
+					if (event.kind().equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
+						Path modified_file = (Path) event.context();
+						if (modified_file.equals(this.watched_file.toPath())) {
+							/*
+							List<String> lines = Files.readAllLines(this.watched_file.toPath());
+							System.out.println(lines);
+							 */
 
+							long whole_file = this.open_file.length();
+							long file_remaining = whole_file - this.last_position;
+							if ( file_remaining > 0 ) {
+
+								byte[] rest_of_file = new byte[(int) file_remaining];
+								this.open_file.read(rest_of_file);
+								String new_part_of_file = new String(rest_of_file, StandardCharsets.UTF_8);
+								System.out.print(new_part_of_file);
+
+								this.last_position = this.open_file.getFilePointer();
+							}
+						}
+					}
+
+
+				}
+
+				key.reset(); //<- when we're done processing each thing that happened, we have to "reset" the watcher so it can listen again
 			}
 
-			key.reset(); //<- when we're done processing each thing that happened, we have to "reset" the watcher so it can listen again
-		}
-
-		} catch ( Exception e ) {
+		} catch (Exception e) {
 			System.err.println("Failed to create file watcher.");
 			e.printStackTrace();
 		}
 
-		
 
 	}
+
 
 }
--- code-lectures/CS200/week10/watching-files/Main.java
+++ code-lectures/CS200/week10/watching-files/Main.java
@@ -10,14 +10,34 @@ By the end of today, you will:
 */
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
 
 public class Main {
 
 	public static void main(String[] args) {
+		File target_file = new File("watched_file.txt");
 
-		FileListener l = new FileListener(new File("watched_file.txt"));
+		FileListener l = new FileListener(target_file);
 
-		l.run();
+
+		Executors.newSingleThreadExecutor().submit(l);
+
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a username: ");
+		String username = s.nextLine();
+
+			while ( true ) {
+				try {
+					String input = username + " >> " + s.nextLine() + "\n";
+					Files.writeString(target_file.toPath(), input, StandardOpenOption.APPEND);
+				} catch ( Exception e) {
+					System.out.println("Failed to write to file:" + e.getMessage());
+				}
+			}
 
 	}
 }
